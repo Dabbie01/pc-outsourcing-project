8e9086c6f57f56c2b1f77199d3c0ea85f5277eee
Dabbie <2310734576@qq.com>
2023-02-20 16:47:34 +0800

    修改格式

--- src/components/aboutxhc/wefocus/wefocus.vue
+++ src/components/aboutxhc/wefocus/wefocus.vue
@@ -1,32 +1,27 @@
 <template>
-  <div>
     <section id="block">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2>我们专注于研究目前亟待解决的医疗需求，利用国际专业的医疗手段，为客户探索最优的解决方案，满足人们对于健康的渴求，改善生活面貌。</h2>
-          </div>
-        </div>
-      </div>
+      <h2>我们专注于研究目前亟待解决的医疗需求，利用国际专业的医疗手段，</h2>
+      <h2>为客户探索最优的解决方案，满足人们对于健康的渴求，改善生活面貌。</h2>
     </section>
-  </div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
   export default {}
 </script>
 
-<style>
+<style scoped>
   /*block start*/
   #block {
+    height: 260px;
+    padding-top: 65px;
     text-align: center;
     background: url("~@/assets/block.png") no-repeat 20% 0% fixed;
   }
+  
   #block h2{
-    padding:80px 40px;
-    font-family: 'qingke';
+    padding-left:80px;
     font-size: 25px;
-    color:white
+    color:white;
   }
   /*block end*/
 
