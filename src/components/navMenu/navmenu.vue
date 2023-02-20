e4763592f17667b90ca89165bba5d83be55893f2
Dabbie <2310734576@qq.com>
2023-02-20 15:31:54 +0800

    头部导航处添加对business和developing业务页面的跳转

--- src/components/navMenu/navmenu.vue
+++ src/components/navMenu/navmenu.vue
@@ -2,7 +2,7 @@
  * @Author: Dabbie 2310734576@qq.com
  * @Date: 2017-05-08 09:23:18
  * @LastEditors: Dabbie 2310734576@qq.com
- * @LastEditTime: 2023-02-20 15:29:08
+ * @LastEditTime: 2023-02-20 15:31:28
  * @FilePath: \vue-xhc-master\src\components\navMenu\navmenu.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -45,4 +45,5 @@
  .nav-item{
    float: right!important;
  }
+ 
 </style>
