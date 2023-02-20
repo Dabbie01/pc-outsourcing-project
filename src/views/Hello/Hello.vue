986f6ca613dd38f5a655408732ec7c0422b36f30
Dabbie <2310734576@qq.com>
2023-02-20 15:33:54 +0800

    删除不必要元素

--- src/views/Hello/Hello.vue
+++ src/views/Hello/Hello.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: Dabbie 2310734576@qq.com
+ * @Date: 2023-02-19 16:12:11
+ * @LastEditors: Dabbie 2310734576@qq.com
+ * @LastEditTime: 2023-02-20 15:33:10
+ * @FilePath: \vue-xhc-master\src\views\Hello\Hello.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="hello">
     <navmenu></navmenu>
@@ -22,7 +30,6 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
