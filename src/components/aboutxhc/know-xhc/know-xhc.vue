7b93b4fc446e5a99934ddab58b45d0e4fc314b6b
Dabbie <2310734576@qq.com>
2023-02-20 16:39:42 +0800

    删除多余元素

--- src/components/aboutxhc/know-xhc/know-xhc.vue
+++ src/components/aboutxhc/know-xhc/know-xhc.vue
@@ -1,80 +1,46 @@
+<!--
+ * @Author: Dabbie 2310734576@qq.com
+ * @Date: 2022-05-08 09:23:18
+ * @LastEditors: Dabbie 2310734576@qq.com
+ * @LastEditTime: 2023-02-20 16:38:13
+ * @FilePath: \vue-xhc-master\src\components\aboutxhc\know-xhc\know-xhc.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div>
     <section id="know">
       <div class="container">
-        <div class="row">
           <h2>认识新合诚</h2>
-          <div class="divider"></div>
-          <p class="Source-Han-Light">新合诚公司致力于通过持续的科技研发，疾病治疗寻求新的解决方案，展示生化科技的力量，交付创新的医疗方法。</p>
-          <p class="Source-Han-Light">我们的技术基于先进的生化多糖科技，冲破疾病预防和治疗的复杂性，为生命健康构筑更多途径。</p>
-          <p class="Source-Han-Light">新合诚公司专注于研究目前待解决的医疗需求，利用国际专业的医疗手段，为客户探索最优的解决方案，满足人们对于健康的渴求，改善生活面貌。</p>
-        </div>
+          <hr>
+          <p>新合诚公司致力于通过持续的科技研发，疾病治疗寻求新的解决方案，展示生化科技的力量，交付创新的医疗方法。</p>
+          <p>我们的技术基于先进的生化多糖科技，冲破疾病预防和治疗的复杂性，为生命健康构筑更多途径。</p>
+          <p>新合诚公司专注于研究目前待解决的医疗需求，利用国际专业的医疗手段，为客户探索最优的解决方案，满足人们对于健康的渴求，改善生活面貌。</p>
       </div>
     </section>
-  </div>
 </template>
 
-<script type="text/ecmascript-6">
+<script>
   export default {}
 </script>
 
-<style>
+<style scoped>
   /*#know start*/
   #know{
-    background-color: white;
     text-align: center;
   }
-  #know .wrap{
-    padding: 10px;
-  }
-  #know .container .row{
-    padding-top: 50px;
-  }
-  #know .divider{
-    width: 10%;
-    background-color: grey;
-    height: 1px;
-    display: inline-block;
-    margin-bottom:40px;
-  }
-
-  #know .row-bottom{
-    padding-bottom: 100px;
-  }
-
+  
   #know h2{
     padding: 10px;
     font-weight: bold;
   }
+
+  #know hr {
+    margin: 10px auto 40px;
+    width: 800px;
+  }
+
   #know p{
     padding: 1px;
     font-size: 16px;
-    font-family: 'Microsoft YaHei';
-  }
-  #know .item-wrap{
-    height: 280px;
-    padding: 10px;
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.3);
-    background-color: white;
-  }
-  #know .item{
-    background-color: #37B7E5;
-    height: 100%;
-    padding: 30px;
-  }
-  #know i {
-    width: 60px;
-    height: 60px;
-    color: white;
-    font-size: 20px;
-    line-height: 60px;
-    border-radius: 50% 50%;
-    border: 2px solid white;
-  }
-  #know .text{
-    padding-top: 50px;
-    color: white;
   }
   /*#know end*/
-
 </style>
